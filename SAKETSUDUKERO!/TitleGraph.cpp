9cb678ff903e610365ea76f1c16cc26f260f2a8f
dorisol1019 <dorisol@hotmail.co.jp>
2016-11-20 23:49:03 +0900

    理想的なタイトル演出にした

--- SAKETSUDUKERO!/TitleGraph.cpp
+++ SAKETSUDUKERO!/TitleGraph.cpp
@@ -15,8 +15,8 @@ void TitleGraph::Move()
 	if (pos.y <= 110 || Input::AnyKeyClicked())
 	{
 		pos.y = 110;
-		update.SetCall(&TitleGraph::Stopping);
-		subject->onNotify(Event::createTitleText,subject);
+		update.SetCall(&TitleGraph::StopingHalfTime);
+		frameCount = 120;
 	}
 
 }
@@ -31,7 +31,44 @@ void TitleGraph::Appear()
 	frameCount++;
 }
 
+void TitleGraph::StopingHalfTime()
+{
+	static int t = 0;
+	if (t / 20 >= 1 || Input::AnyKeyClicked())
+	{
+		update.SetCall(&TitleGraph::SceleExclamation);
+		draw.SetCall(&TitleGraph::DrawTitle);
+	}
+	t++;
+}
+
+void TitleGraph::SceleExclamation()
+{
+	constexpr double scaleSpeed = 0.5;
+	constexpr int speed = 2;
+	pos.x -= speed;
+	scale -= scaleSpeed;
+
+	if (pos.x <= Window::Center().x - 40 || Input::AnyKeyClicked())
+	{
+		pos.x = Window::Center().x - 40;
+		if (scale <= 1.0 || Input::AnyKeyClicked())
+		{
+			scale = 1.0;
+			update.SetCall(&TitleGraph::Stopping);
+			subject->onNotify(Event::createTitleText, subject);
+		}
+
+	}
+}
+
 void TitleGraph::Draw()
 {
-	texture.drawAt(pos, AlphaF((double)(frameCount) / (2 * 60)));
+	texture.drawAt(pos, AlphaF(Clamp((double)(frameCount) / (2 * 60), 0.0, 1.0)));
+}
+
+void TitleGraph::DrawTitle()
+{
+	texture.drawAt(pos);
+	texture2.scale(scale).drawAt(pos2);
 }
--- SAKETSUDUKERO!/TitleGraph.h
+++ SAKETSUDUKERO!/TitleGraph.h
@@ -11,6 +11,8 @@ class TitleGraph :
 private:
 	Vec2 pos;
 	Texture texture;
+	Vec2 pos2;
+	Texture texture2;
 
 	Subject*subject;
 
@@ -18,22 +20,28 @@ private:
 	TaskCall draw;
 
 	int frameCount = 0;
+	double scale;
 public:
 	TitleGraph(Subject*subject) :
-		update(this, &TitleGraph::Appear,TaskCallGroup::Update),
-		draw(this,&TitleGraph::Draw,TaskCallGroup::Draw),
-		pos(Window::Center()), texture(L"Data/System/Title.png"),
+		update(this, &TitleGraph::Appear, TaskCallGroup::Update),
+		draw(this, &TitleGraph::Draw, TaskCallGroup::Draw),
+		pos({ Window::Center().x,Window::Center().y }), texture(L"Data/System/Title.png"),
+		pos2({ 800,107 }), texture2(L"Data/System/Title2.png"), scale(10),
 		subject(subject)
 	{}
-	
+
 	~TitleGraph();
 
 private:
-	
+
 	void Stopping();
 	void Move();
 	void Appear();
+	void StopingHalfTime();
+	void SceleExclamation();
+
 	void Draw();
+	void DrawTitle();
 
 
 };
